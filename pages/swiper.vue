a9347b8b96ada5158f28027a524f4ac29ea65471
Tráng Hà Viết <viettrangha@gmail.com>
2019-03-27 00:53:46 +0700

    add swiper carousel

---
+++ pages/swiper.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <swiper ref="mySwiper" :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide>
+        <img
+          src="http://static.asiawebdirect.com/m/bangkok/portals/vietnam/homepage/hanoi/pagePropertiesOgImage/teaser_006.jpg.jpg">
+      </swiper-slide>
+      <swiper-slide>
+        <img
+          src="http://static.asiawebdirect.com/m/bangkok/portals/vietnam/homepage/hanoi/pagePropertiesOgImage/teaser_006.jpg.jpg">
+      </swiper-slide>
+      <swiper-slide>
+        <img
+          src="http://static.asiawebdirect.com/m/bangkok/portals/vietnam/homepage/hanoi/pagePropertiesOgImage/teaser_006.jpg.jpg">
+      </swiper-slide>
+      <swiper-slide>
+        <img
+          src="http://static.asiawebdirect.com/m/bangkok/portals/vietnam/homepage/hanoi/pagePropertiesOgImage/teaser_006.jpg.jpg">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div slot="pagination" class="swiper-pagination" />
+      <div slot="button-prev" class="swiper-button-prev" />
+      <div slot="button-next" class="swiper-button-next" />
+      <!--<div slot="scrollbar" class="swiper-scrollbar" />-->
+    </swiper>
+  </div>
+</template>
+
+<script>
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+
+export default {
+  name: 'SwiperCarousel',
+  components: {
+    swiper,
+    swiperSlide
+  },
+  data() {
+    return {
+      swiperOption: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      }
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+  .swiper-slide {
+    img {
+      max-width: 100%;
+    }
+  }
+</style>
